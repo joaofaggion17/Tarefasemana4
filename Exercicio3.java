12bd04ee62c64c5fd97d02733655904d5c0f0e70
joaofaggion17 <joaofaggion17@gmail.com>
2022-04-05 02:49:17 +0000

    Salvando

---
+++ Exercicio3.java
@@ -0,0 +1,8 @@
+public class Exercicio3 {
+    public static void main(String[] args){
+        for (int a = 1; a <= 100; a++)
+            if (a % 3 == 0){
+                System.out.println(a);
+            }
+        }
+    }
