062fe752f34859c4ea75fb42a4ed1c5f866f798b
joaofaggion17 <joaofaggion17@gmail.com>
2022-04-05 02:42:30 +0000

    Salvando

---
+++ Exercicio1.java
@@ -0,0 +1,9 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+
+        for (int x = 150; x <= 300; x++){
+            System.out.println("Valor: " + x);
+        }
+    }
+    
+}
